7e694ca580ea1c4668a132f39f3dcf2d131ba406
Pablo <ezekielgonzalezr@gmail.com>
2023-09-19 22:20:28 -0600

    Fix rubocop offeses

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,7 @@
 class GreetingsController < ApplicationController
-    def index
-        greetings = Greeting.all
-        @greeting = greetings[rand(greetings.size)]
-        render json: @greeting
-    end
+  def index
+    greetings = Greeting.all
+    @greeting = greetings[rand(greetings.size)]
+    render json: @greeting
+  end
 end
