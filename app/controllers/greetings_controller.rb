4cf0c287f70aef3e49870a1ecb641901369c2476
Pablo <ezekielgonzalezr@gmail.com>
2023-09-19 22:12:13 -0600

    Render json

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -2,5 +2,6 @@ class GreetingsController < ApplicationController
     def index
         greetings = Greeting.all
         @greeting = greetings[rand(greetings.size)]
+        render json: @greeting
     end
 end
